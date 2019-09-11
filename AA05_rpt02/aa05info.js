7d530f19967034d577ca17cde48a255dcd449c3b
Ryu jae hyeon <54879721+Inje-AA05@users.noreply.github.com>
2019-09-11 16:55:24 +0900

    Add files via upload

---
+++ AA05_rpt02/aa05info.js
@@ -0,0 +1,10 @@
+//aa05info.js
+
+module.exports = function(id, name, number, phone, major){
+	console.log("Id = " + id);
+	console.log("name = " + name);
+	console.log("number = " + number);
+	console.log("phone = " + phone);
+	console.log("major = " + major);
+
+}
---
+++ AA05_rpt02/index_aa05.js
@@ -0,0 +1,7 @@
+//index.aa05.js
+
+var myinfo = require('./aa05info');
+
+myinfo("aa05", "류재현" , "20151530" , "010-2424-1530" , "Comsi");
+
+myinfo("AA05", "Ryujaehyeon" , "20151530", "0000000000" , "Drone");
